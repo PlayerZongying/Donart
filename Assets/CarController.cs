98ab594ca9207ed0bdeabb74426eaecc485c4d13
PlayerZongying <playerzongying@gmail.com>
2023-09-25 19:59:27 +0200

    change parameters

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -15,6 +17,8 @@ public class CarController : MonoBehaviour
 
     [Header("Car Steering")] [Range(0f, 1f)]
     public float wheelGripFactor = 1f;
+
+    public AnimationCurve wheelGripFactorCurve;
     public float maxRotationAngle = 30;
     public float wheelMass = 10f;
 
@@ -22,6 +26,7 @@ public class CarController : MonoBehaviour
     public float accelInput;
     public float accelFactor;
     public float maxSpeed;
+    public float frictionFactor = 0;
     public float accelarationFactor = 30f;
 
     public float turnFactor = 2.5f;
@@ -40,6 +45,10 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetCar();
+        }
     }
 
     private void FixedUpdate()
@@ -51,17 +60,20 @@ public class CarController : MonoBehaviour
                     LayerMask.GetMask("Default")))
             {
                 // suspension
-                Debug.DrawRay(wheelTransform.position, -wheelTransform.up * hit.distance, Color.green);
+                Debug.DrawRay(wheelTransform.position, -wheelTransform.up * hit.distance, Color.yellow);
                 float offset = suspensionDist - hit.distance;
                 float velocity = Vector3.Dot(wheelTransform.up, carRigidBody.GetPointVelocity(wheelTransform.position));
                 float force = (offset * suspensionStrength) - (velocity * suspensionDamping);
                 carRigidBody.AddForceAtPosition(wheelTransform.up * force, wheelTransform.position);
+                Debug.DrawRay(wheelTransform.position, wheelTransform.up * force, Color.green);
 
                 // steering
                 Vector3 steeringDir = wheelTransform.right;
 
                 Vector3 wheelWorldVel = carRigidBody.GetPointVelocity(wheelTransform.position);
                 float steeringVel = Vector3.Dot(steeringDir, wheelWorldVel);
+                float normalizedSpped = carRigidBody.velocity.magnitude / maxSpeed;
+                wheelGripFactor = wheelGripFactorCurve.Evaluate(normalizedSpped);
                 float desiredVelChange = -steeringVel * wheelGripFactor;
 
                 float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
@@ -75,7 +87,13 @@ public class CarController : MonoBehaviour
                 float carSpeed = Vector3.Dot(transform.forward, carRigidBody.velocity);
                 float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / maxSpeed);
                 float availableTorque = (1 - normalizedSpeed) * accelInput * accelFactor;
-                carRigidBody.AddForceAtPosition(accelDir * availableTorque, wheelTransform.position);
+                float frictionMagnitute = carRigidBody.mass * Vector3.Dot(transform.up, Vector3.up) * frictionFactor;
+                Vector3 friction = - accelDir * frictionMagnitute;
+                if (Vector3.Dot(carRigidBody.velocity, accelDir) < 0)
+                {
+                    friction *= -1;
+                }
+                carRigidBody.AddForceAtPosition(accelDir * availableTorque + friction, wheelTransform.position);
                 
                 
                 
@@ -110,5 +128,19 @@ public class CarController : MonoBehaviour
     {
         accelInput = inputVector.y;
         steeringInput = inputVector.x;
+    }
+
+    private void ResetCar()
+    {
+        transform.position = Vector3.zero;
+        transform.position +=  Vector3.up * 5;
+
+        Vector3 targetUp = Vector3.up;
+        Vector3 targetRight = Vector3.Cross(targetUp, transform.forward);
+        Vector3 targetForward = Vector3.Cross(targetRight, targetUp);
+        
+        transform.rotation = Quaternion.LookRotation(targetForward,targetUp);
+        carRigidBody.velocity = Vector3.zero;
+        carRigidBody.angularVelocity = Vector3.zero;
     }
 }
